a00746a4138861598e3b095cff5ffc711a200cf3
Gregoire Puscian <g.puscian@gmail.com>
2025-08-27 14:36:13 +0200

    Initial commit - Medical Planner App

---
+++ components/MedicalPlanner.jsx
@@ -0,0 +1,5 @@
+// Copier le code frontend React de l'artefact précédent
+// (Code complet fourni séparément pour éviter la longueur)
+export default function MedicalPlannerApp() {
+  // Code React complet ici
+}
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+import MedicalPlannerApp from '../components/MedicalPlanner'
+
+export default function Home() {
+  return <MedicalPlannerApp />
+}
